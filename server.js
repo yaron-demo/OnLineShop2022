bc236c2f66ce68e35186af0a2520992fd64da4b7
Yaron Lapidot <ylapidot@gmail.com>
2022-08-02 18:52:22 +0300

    New Version 1.2.3

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 const http=require('http');
+const app=require('./app');
+const server=http.createServer(app);
+const port=process.env.PORT || 8787
+server.listen(port,()=>{
+
+    console.log('Server On Air');
+});
